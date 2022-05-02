3fd0dc5b68d0453b0d2104621ecb943b51914592
sashaant <antonenko.qa@gmail.com>
2022-05-02 20:22:30 +0300

    init repo

---
+++ playwright.config.ts
@@ -0,0 +1,14 @@
+// playwright.config.ts
+import { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  use: {
+    screenshot: 'only-on-failure'
+  },
+  reporter: [
+    ['line'], 
+    ['experimental-allure-playwright']
+  ],
+};
+
+export default config;
